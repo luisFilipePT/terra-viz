7a543fd451e46fa2563bce05c4736cf7e94faef1
Luis <luis.fgbo@gmail.com>
2021-10-08 17:45:39 +0100

    Refactor images

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
       </header>
       <main>
           <div className="swarm-container">
-            {/*<div style={{ display: 'flex', flexDirection: 'column', paddingTop: 60, justifyContent: 'space-around'}}>*/}
-            {/*  <img width="320px" src="https://starterra.io/image/Tier__03.png"/>*/}
-            {/*  <img width="320px" src="https://starterra.io/image/Tier__02.png"/>*/}
-            {/*  <img width="320px" src="https://starterra.io/image/Tier__04.png"/>*/}
-            {/*</div>*/}
+            <div style={{ display: 'flex', flexDirection: 'column', paddingTop: 60, justifyContent: 'space-around'}}>
+              <img width="320px" src="https://app.starterra.io/static/media/degens-banner.4bf98705.png"/>
+              <img width="320px" src="https://app.starterra.io/static/media/interstellars-banner.3c689592.png"/>
+              <img width="320px" src="https://app.starterra.io/static/media/lunatics-banner.d4b8ebc9.png"/>
+            </div>
             <SwarmPlot/>
           </div>
       </main>
