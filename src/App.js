fb511bd17c1831569dd53623d8dff356217db352
Luis <luis.fgbo@gmail.com>
2021-10-09 15:40:08 +0100

    Add template

--- src/App.js
+++ src/App.js
@@ -5,23 +5,33 @@ import StarTerraLogo from "./components/StarTerraLogo";
 import Section2 from "./sections/section2";
 import Section3 from "./sections/section3";
 import Section4 from "./sections/section4";
+import {Container, Content, FlexboxGrid, Footer, Header, Navbar} from "rsuite";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Particles />
-      <header className="App-header">
-        <StarTerraLogo />
-      </header>
-      <main>
-        <Section2/>
-        <Section3/>
-        <Section4/>
-        <div className="table-wrapper">
-          <DataTable />
-        </div>
-      </main>
-    </div>
+      <Container>
+        <Header>
+          <Navbar>
+            <Navbar.Header>
+              <div className="App-header">
+                <StarTerraLogo />
+              </div>
+            </Navbar.Header>
+          </Navbar>
+        </Header>
+        <Content>
+          <Section2/>
+          <Section3/>
+          <Section4/>
+          <div className="table-wrapper">
+            <DataTable />
+          </div>
+        </Content>
+        <Footer>Footer</Footer>
+      </Container>
+    </>
   );
 }
 
